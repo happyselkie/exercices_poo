3942d27c7d3320547be556326f30aa9793fd7e11
fbanier <frbanier@gmail.com>
2025-05-14 13:40:21 +0200

    exercice3 : joueur

--- src/main/java/org/example/exercice3/Main.java
+++ src/main/java/org/example/exercice3/Main.java
@@ -4,20 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Joueur joueur1 = new Joueur("WarriorDu59", 1, 0);
-        Joueur joueur2 = new Joueur("JustMe", 16, 90);
-        Joueur joueur3 = new Joueur("Reroll1254", 2, 80);
-
-        /*System.out.println(joueur2);
-
-        joueur2.effectuerUneQuete();
-
-        System.out.println(joueur2);
-        System.out.println(joueur3);
-
-        joueur3.effectuerUneQuete();
-        joueur3.effectuerUneQuete();
-
-        System.out.println(joueur3);*/
 
         String nomJoueur1 = joueur1.getNom();
         for(int i = 1; i <= 20; i++){
