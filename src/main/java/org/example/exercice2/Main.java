3be23421fb77b278d657707ff6a5d44d7c01f683
fbanier <frbanier@gmail.com>
2025-05-14 12:16:58 +0200

    exercice2 : film

--- src/main/java/org/example/exercice2/Main.java
+++ src/main/java/org/example/exercice2/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Film film1 = new Film("La Haine", "Mathieu Kassovitz","1995-05-31", "Drame");
         Film film2 = new Film("Avatar 2", "James Cameron","2022-12-14", "Action");
+
+        System.out.println(film1);
+        System.out.println(film2);
     }
 }
